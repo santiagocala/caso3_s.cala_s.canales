e28496c1d5893784af27cb2a8bd8b1a805348d49
santiagocala <s.cala@uniandes.edu.co>
2019-11-14 12:30:14 -0500

    Ya casi terminado el protocolo sin seguridad del cliente

--- src/ClienteNoSeguro.java
+++ src/ClienteNoSeguro.java
@@ -1,8 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
 
 public class ClienteNoSeguro {
 	
@@ -10,6 +17,12 @@ public class ClienteNoSeguro {
 	private static PrintWriter pw;
     private static BufferedReader bf;
     private static InputStreamReader in;
+    private static String ALGORITMOS = "ALGORITMOS";
+    private static String ASIMETRICO = "RSA";
+    private static String SIMETRICO = "AES";
+    private static String HMAC = "HMACSHA256";
+    private static KeyGenerator keyGen;
+    
 	
 	public static void main(String[] args){
 		// Ver protocolo para entender la transferencia de mensajes. 
@@ -20,7 +33,61 @@ public class ClienteNoSeguro {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+		// Se envían los algortimos de cifrado, simétrico, asimétrico y MAC 
+		pw.println(ALGORITMOS +  ":" + SIMETRICO + ":" + ASIMETRICO + ":" + HMAC);
+		String mensaje = "";
+		try{
+			mensaje = bf.readLine();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		// Verifica que el servidor haya respondido "OK" a los algoritmos que se le enviaron. 
+		if(mensaje == "ERROR"){
+			System.out.println("Se produjo un error después de mandar los algoritmos");
+		}
+		// Recibe el certificado digital
+		try{
+			// Acá se recibe el certificado, pero en realidad no se hace nada con él
+			bf.readLine();
+			// Se genera una llave simétrica y se manda por el canal y después el reto. 
+            SecretKey sk = generateSimetricKey();
+            pw.println(sk);
+            pw.println("reto");
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		//Variable que tomará el valor del mensaje a mandar.
+		//Depende de si el mensaje recibido es el correcto o no. 
+		String respuesta = "";
+		try{
+			mensaje = bf.readLine();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		// Verifica que el servidor haya respondido el mismo reto que enviaron. 
+		if(mensaje != "reto"){
+			respuesta = "ERROR";
+			System.out.println("Se produjo un error después de leer el reto");
+		}
+		else{
+			respuesta = "OK";
+		}
+		pw.println(respuesta);
+		// Se abre un canal por donde el usuario puede ingresar su cédula y contraseña
+		Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su cédula: ");
+        String cedula = scanner.nextLine();
+        System.out.println("Ingrese su contraseña: ");
+        String contrasena = scanner.nextLine();
+        //Se envían la cédula y la contraseña por el canal de comunicación
+        pw.println(cedula);
+        pw.println(contrasena);
+        scanner.close();
+        try{
+        	String valor = bf.readLine();
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
 		
 	}
 
@@ -37,5 +104,17 @@ public class ClienteNoSeguro {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+	
+	/**
+	 * Método que se encarga de generar una llave simétrica secreta
+	 */
+	private static SecretKey generateSimetricKey(){
+        try {
+            keyGen = KeyGenerator.getInstance(SIMETRICO);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return keyGen.generateKey();
     }
 }
